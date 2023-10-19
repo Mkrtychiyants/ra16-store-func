cdd1047e4a34a81be4a169eee432d3ea74c975a5
Mkrtychiyants <heisaloosershesad@gmail.com>
2023-10-19 18:19:56 +1000

    toFixed fix

--- src/components/ShopItemFunc/ShopItemFunc.jsx
+++ src/components/ShopItemFunc/ShopItemFunc.jsx
@@ -12,8 +12,8 @@ export const ShopItemFunc = (props) => {
             <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
             <div className="divider"></div>
             <div className="purchase-info">
-                <div className="price">{item.currency}{item.price}</div>
-                <button>Добавить в корзину</button>
+                <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+                <button>Добавить в корзину</button>     
             </div>
         </div>
     )
